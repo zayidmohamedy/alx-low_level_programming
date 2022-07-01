cde4d3d69be39b3f0a2510da423f9000e6baaf06
zayid mohamed <sayidka843@gmail.com>
2022-07-01 04:36:31 -0700

    add

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-#include<conio.h>
 int main(void){
 char ch;
-for(ch='a';ch <= 'z';ch++){
-    if( ch !='e' &  ch !='q'){
-            putchar(ch);
-        } else{
-             ch++;
+for(ch='a';ch <= 'z';ch++)
+{
+	if( ch !='e' & ch != 'e')
+	{
+		putchar(ch);
         }
-
-   putchar('\n');
+	else
+	{
+		ch++;
+        }
 }
-
+putchar('\n');
 return 0;
 }
 
